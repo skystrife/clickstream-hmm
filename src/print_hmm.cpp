e17b90c98e8b8009a2cc0a24e46bbffbbdf9fb2b
Chase Geigle <geigle1@illinois.edu>
2016-10-21 12:46:05 -0500

    Make print-hmm also output JSON for plotting.

--- src/print_hmm.cpp
+++ src/print_hmm.cpp
@@ -3,10 +3,13 @@
  * Prints the distributions for a HMM model file.
  */
 
+#include "json.hpp"
+
 #include "meta/io/gzstream.h"
 #include "meta/sequence/hmm/hmm.h"
 #include "meta/sequence/hmm/sequence_observations.h"
 
+using namespace nlohmann;
 using namespace meta;
 
 util::string_view action_name(sequence::state_id aid)
@@ -27,9 +30,15 @@ util::string_view action_name(sequence::state_id aid)
     return actions[aid];
 }
 
-int main()
+int main(int argc, char** argv)
 {
     logging::set_cerr_logging();
+
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " human|json" << std::endl;
+        return 1;
+    }
 
     using namespace sequence;
     using namespace hmm;
@@ -39,29 +48,60 @@ int main()
 
     const auto& obs_dist = hmm.observation_distribution();
 
-    for (state_id sid{0}; sid < obs_dist.num_states(); ++sid)
+    if (argv[1] == util::string_view{"human"})
     {
-        std::cout << "HMM State " << sid << ":\n"
-                  << "=========\n";
-        const auto& mm = obs_dist.distribution(sid);
+        for (state_id sid{0}; sid < obs_dist.num_states(); ++sid)
+        {
+            std::cout << "HMM State " << sid << ":\n"
+                      << "=========\n";
+            const auto& mm = obs_dist.distribution(sid);
 
-        std::cout << "Markov Model Initial probs:\n";
-        for (state_id init{0}; init < mm.num_states(); ++init)
-        {
-            std::cout << "\"" << action_name(init) << "\":\t"
-                      << mm.initial_probability(init) << "\n";
-        }
-        std::cout << "\n";
-        std::cout << "Markov Model Transition probs:\n";
-        for (state_id i{0}; i < mm.num_states(); ++i)
-        {
-            for (state_id j{0}; j < mm.num_states(); ++j)
+            std::cout << "Markov Model Initial probs:\n";
+            for (state_id init{0}; init < mm.num_states(); ++init)
             {
-                std::cout << action_name(i) << " -> " << action_name(j) << ": "
-                          << mm.transition_probability(i, j) << "\n";
+                std::cout << "\"" << action_name(init) << "\":\t"
+                          << mm.initial_probability(init) << "\n";
             }
             std::cout << "\n";
+            std::cout << "Markov Model Transition probs:\n";
+            for (state_id i{0}; i < mm.num_states(); ++i)
+            {
+                for (state_id j{0}; j < mm.num_states(); ++j)
+                {
+                    std::cout << action_name(i) << " -> " << action_name(j)
+                              << ": " << mm.transition_probability(i, j)
+                              << "\n";
+                }
+                std::cout << "\n";
+            }
         }
+    }
+    else if (argv[1] == util::string_view{"json"})
+    {
+        for (state_id sid{0}; sid < obs_dist.num_states(); ++sid)
+        {
+            auto arr = json::array();
+
+            const auto& mm = obs_dist.distribution(sid);
+            for (state_id i{0}; i < mm.num_states(); ++i)
+            {
+                auto trans = json::array();
+                for (state_id j{0}; j < mm.num_states(); ++j)
+                {
+                    trans.push_back(mm.transition_probability(i, j));
+                }
+
+                arr.push_back({{"name", action_name(i).to_string()},
+                               {"init", mm.initial_probability(i)},
+                               {"edges", trans}});
+            }
+            std::cout << arr << "\n";
+        }
+    }
+    else
+    {
+        LOG(fatal) << "Unknown output format type" << ENDLG;
+        return 1;
     }
 
     return 0;
