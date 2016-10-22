d7bad5c12a624821b60b8d41e0d2263782f27aaf
Chase Geigle <geigle1@illinois.edu>
2016-10-21 23:10:54 -0500

    Add script to plot each of the hidden states.

---
+++ scripts/plot_models.py
@@ -0,0 +1,28 @@
+import cairocffi as cairo
+from igraph import *
+import fileinput
+import json
+import math
+
+g = 0
+layout = None
+for line in fileinput.input():
+    nodes = json.loads(line)
+    graph = Graph(directed=True)
+
+    for i, node in enumerate(nodes):
+        graph.add_vertex(i, label=node['name'], weight=node['init'])
+
+    for i, node in enumerate(nodes):
+        for j, edge in enumerate(node['edges']):
+            graph.add_edge(i, j, weight=edge)
+
+    plot(graph,
+         bbox=(1000, 1000),
+         edge_width=[5 * weight for weight in graph.es['weight']],
+         vertex_size=[200 * weight for weight in
+             graph.personalized_pagerank(weights='weight', reset='weight')],
+         target="state{}.png".format(g),
+         layout=graph.layout_circle(), margin=100, vertex_label_dist=1)
+
+    g += 1
